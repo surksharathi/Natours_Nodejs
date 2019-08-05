bfc2253c6b20444cf761302909172b03ce26ab1c
surksha rathi <surksha8@gmail.com>
2019-08-05 11:05:30 +0530

    post api request complete

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
 const Express = require('express');
-
+const fs = require('fs');
 const app = Express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Welcome to the node course world', app: 'Naturesh' });
+app.use(Express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'Success',
+    result: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(tours);
+  const newId = tours[tours.length - 1].id + 1;
+  console.log(tours[tours.length - 1].id + 1);
+  const newTours = Object.assign({ id: newId }, req.body);
+  tours.push(newTours);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTours
+        }
+      });
+    }
+  );
+  console.log('Done');
 });
 app.listen(port, () => {
   console.log(`App is running on port ${port}.......`);
